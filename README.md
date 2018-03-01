402cc9478719756c46fa31a509f7d8ed4a19e9ca
Joshua Wickings <joshuadoespersonalstuff@gmail.com>
2018-03-01 14:18:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# go-soundcloud
+Single file library for accessing SoundCloud metadata and audio URLs in Golang without extra dependencies
+
+# Installing
+`go get github.com/JoshuaDoes/go-soundcloud`
+
+# Example
+```go
+package main
+
+import "fmt"
+import "github.com/JoshuaDoes/go-soundcloud"
+
+func main() {
+	//Initialize a new client
+	c := &soundcloud.Client{ClientID:"your client id here", AppVersion:"your app version here"}
+
+	//Get metadata and an audio URL
+	res, err := c.GetTrackInfo("https://soundcloud.com/artist/title")
+
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Println("Track ID: " + res.Track)
+	fmt.Println("Title: " + res.Title)
+	fmt.Println("Artist: " + res.Artist)
+	fmt.Println("Description: " + res.Description)
+	fmt.Println("Download URL: " + res.DownloadURL)
+	fmt.Println("Artwork URL: " + res.ArtURL)
+}
+```
+### Output
+
+```
+> go run main.go
+
+< Track ID
+< Title
+< Artist
+< Description
+< Download URL
+< Artwork URL
+```
+
+## License
+The source code for go-ppm is released under the MIT License. See LICENSE for more details.
+
+## Donations
+All donations are appreciated and helps me stay awake at night to work on this more. Even if it's not much, it helps a lot in the long run!
+You can find the donation link here: [Donation Link](https://paypal.me/JoshuaDoes)
---
+++ soundcloud.go
@@ -0,0 +1,95 @@
+package soundcloud
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+type Client struct {
+	ClientID string
+	AppVersion string
+}
+
+type Track struct {
+	Artist string
+	ArtURL string
+	Description string
+	DownloadURL string
+	Title string
+	Track string
+}
+
+func (c *Client) GetTrackInfo(url string) (*Track, error) {
+	request, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer request.Body.Close()
+	buffer := new(bytes.Buffer)
+	buffer.ReadFrom(request.Body)
+	html := buffer.String()
+
+	regex := regexp.MustCompile("soundcloud:tracks:([0-9]+)")
+	track := regex.FindStringSubmatch(html)
+	if len(track) <= 0 {
+		return nil, errors.New("Error finding track ID")
+	}
+	regex = regexp.MustCompile("[\"']secret_token[\"'] *: *[\"'](.-)[\"']")
+	secret := regex.FindStringSubmatch(html)
+	//regex = regexp.MustCompile("[\"']title[\"'] *: *\"(.-[^\\])\"")
+	regex = regexp.MustCompile("(?s)\"title\":\"(.*?)\"")
+	title := regex.FindStringSubmatch(html)
+	if len(title) <= 0 {
+		return nil, errors.New("Error finding title")
+	}
+	regex = regexp.MustCompile("(?s)\"description\":\"(.*?)\"")
+	description := regex.FindStringSubmatch(html)
+	if len(description) <= 0 {
+		return nil, errors.New("Error finding description")
+	}
+	regex = regexp.MustCompile("(?s)\"username\":\"(.*?)\"")
+	artist := regex.FindStringSubmatch(html)
+	if len(artist) <= 0 {
+		return nil, errors.New("Error finding artist")
+	}
+	regex = regexp.MustCompile("(?s)\"artwork_url\":\"(.*?)\"")
+	artURL := regex.FindStringSubmatch(html)
+	if len(artURL) <= 0 {
+		return nil, errors.New("Error finding artwork URL")
+	}
+
+	url = fmt.Sprintf("https://api.soundcloud.com/i1/tracks/%s/streams?client_id=%s&app_version=%s", track[1], c.ClientID, c.AppVersion)
+	if len(secret) > 0 {
+		url += "&secret_token=" + secret[1]
+	}
+	
+	data := &Track{}
+	result, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer result.Body.Close()
+	buffer = new(bytes.Buffer)
+	buffer.ReadFrom(result.Body)
+	downloadData := buffer.String()
+
+	regex = regexp.MustCompile("(?s)\"http_mp3_128_url\":\"(.*?)\"")
+	mp3128URL := regex.FindStringSubmatch(downloadData)
+	if len(mp3128URL) <= 0 {
+		return nil, errors.New("Error finding download URL")
+	}
+	downloadURL := strings.Replace(mp3128URL[1], "\\u0026", "&", -1)
+
+	data.Artist = artist[1]
+	data.ArtURL = artURL[1]
+	data.Description = description[1]
+	data.DownloadURL = downloadURL
+	data.Title = title[1]
+	data.Track = track[1]
+	
+	return data, err
+}
